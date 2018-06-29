c6116d8f6ee62a9b4a95ba11fb75f2e716eb170a
Tony <diablotony01@hotmail.com>
2018-06-28 19:54:15 -0700

    added footer text

--- themes/food-colour/footer.php
+++ themes/food-colour/footer.php
@@ -12,16 +12,22 @@
 			<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="footer-info-container">
 						<div class="footer-text">
-							<h3>Liu institute For Global Issues</h3>
 								<p class="social-media-icons">
 									<i class="fa fa-facebook-square" aria-hidden="true"></i>
 									<i class="fa fa-youtube"></i>
 									<i class="fa fa-twitter-square" aria-hidden="true"></i>
 									<i class="fa fa-linkedin"></i>
 								</p>						
+							<h3>Liu institute For Global Issues</h3>
+							<p>6476 NW Marine Drive
+							Vancouver, BC, Canada, V6T 1Z2
+							Tel 604 822 1558
+							Fax 604 822 6966</p>
+							
+							<p>All copyrights reserved to LUGE - 2018</p>
 					</div>
-				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
+				</div><!-- .site-info -->
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
