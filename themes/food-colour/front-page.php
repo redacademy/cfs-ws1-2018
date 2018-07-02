7e148bd89962a515e6d7a9a05f730ec520e21578
Lucas <lucaspereiracunha08@gmail.com>
2018-07-02 14:20:53 -0700

    commit front page

--- themes/food-colour/front-page.php
+++ themes/food-colour/front-page.php
@@ -8,27 +8,44 @@
  * 
  */
 get_header(); ?>
-
 	<div id="primary" class="site-content">
 		<div class='image-wrapper'>
 			<img class='background-image-main' src="<?php echo esc_url(get_bloginfo('template_directory')); ?>/assets/images/home_background.jpg" alt="home background"/>
 			<img class='background-map-main' src="<?php echo esc_url(get_bloginfo('template_directory')); ?>/assets/images/map_vector_hero.png" alt="home background"/>
 			<div id="content" role="main">
-				<section class='title-1'>
-					<h1>THE COLOURS OF FOOD SECURITY</h1>
-					<p>The Colours of Food Security is a series of bold and striking maps that paint a picture of global agriculture, and key issues surrounding the food system.The message of this exhibit is that to achieve food secure future we need new visions that view the food system from all its many angles, tones and colours.</p>
-					<button>
-				</section>
-				<section class='title-2'>
-					<h1>What is Food Security?</h1>
-					<p>Food security is a complex issue integrating social and environmental dimensions. Our future access and utilization of stable supplies of quality food depend on us solving farmer livelihood, agricultural productivity, poverty, climate, human health, biodiversity loss, water, and environmental pollution. </p>
-					<button>
-				</section>
-				<section class='title-3'>
-					<h1>Take part</h1>
-					<p>Want to improve global food security and sustainability? You can host "The colours of food security" exhibit. Just download the images and print it out.</p>
-					<button>
-				</section>
+			
+			<?php 
+				$fptitle_1 = CFS()->get('fp_title_1');
+				$fptext_1 = CFS()->get('fp_text_1');
+				$fptitle_2 = CFS()->get('fp_title_2');
+				$fptext_2 = CFS()->get('fp_text_2');
+				$fpimage_2 = CFS()->get('fp_image_2');
+				$fptitle_3 = CFS()->get('fp_title_3');
+				$fptext_3 = CFS()->get('fp_text_3');
+				$fptext_3 = CFS()->get('fp_text_3');
+				$fpimage_3 = CFS()->get('fp_image_3');
+
+				echo "<section class='title-1'>";
+				echo "<h1>" .  $fptitle_1 . "</h1>";
+					echo "<p>" . $fptext_1 . "</p>";
+					echo "<button>";
+				echo "</section>";
+				echo "<section class='title-2'>";
+					echo '<img src="'.esc_url($fpimage_2).'" rel="lightbox">'; 
+					echo "<div class='side-text'>";
+					echo "<h1>" . $fptitle_2 . "</h1>";
+					echo "<p> . $fptext_2 . </p>";
+					echo "<button>";
+					echo "</div>";
+				echo "</section>";
+				echo "<section class='title-3'>";
+					echo "<h1>" . $fptitle_3 . "</h1>";
+					echo "<p> . $fptext_3 . </p>";
+					echo "<button>";
+					echo '<img src="'.esc_url($fpimage_3).'" rel="lightbox">'; 
+				echo "</section>";
+				
+			?>
 
 			</div><!-- #content -->
 		</div>
