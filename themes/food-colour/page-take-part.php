e1c81a881183b4c8964356fbded8c0d4f2926d08
Tony <diablotony01@hotmail.com>
2018-07-03 01:48:49 -0700

    fixed take part page to display correct texts

--- themes/food-colour/page-take-part.php
+++ themes/food-colour/page-take-part.php
@@ -6,7 +6,7 @@
 <body id="primary" class="take-content-area">
     <?php get_header(); ?>
 
-        <section class="hero-image">
+        <section class="icon-image">
             <?php if ( have_posts() ) : ?>
                 <?php if ( is_home() && ! is_front_page() ) : ?>
                 <?php endif; ?>
@@ -22,71 +22,75 @@
             ?>
         </section>
 
-    <div class="entry-content">
+        <?php
 
-        <?php	$args = array( 'numberposts' => '5', 'order' => 'ASC');
-			$take_part_posts = get_posts( $args ); ?>
-
-            <?php foreach ( $take_part_posts as $post ) : setup_postdata( $post );?>
-            <article class="journal-entry">
-            <?php the_post_thumbnail( 'medium' ); ?>
-            </article>
-            <?php endforeach; wp_reset_postdata(); ?>
-                    
-        <?php  
-
-        //     $image = CFS()->get('image');
-        //     $title = CFS()->get('title');
-        //     $title_text = CFS()->get('title_text');
-        //     $image_2 = CFS()->get('image_2');
-        //     $title_2 = CFS()->get('title_2');
-        //     $title_2_text = CFS()->get('title_2_text');
-        //     $image_3 = CFS()->get('image_3');
-        //     $title_3 = CFS()->get('title_3');
-        //     $title_3_text = CFS()->get('title_3_text');
-        //     $image_4 = CFS()->get('image_4');
-        //     $title_4 = CFS()->get('title_4');
-        //     $title_4_text = CFS()->get('title_4_text');
-        //     $image_5 = CFS()->get('image_5');
-        //     $title_5 = CFS()->get('title_5');
-        //     $title_5_text = CFS()->get('title_5_text');
-        //     echo "<div class='take-text-wrapper'>";
-        //     echo "<div class='icons-title'>";
-        //     echo "<img src=" . $image . ">";
-        //     echo "<h2>" . "<a>" . $title . "</a>" . "</h2>";
-        //     echo "</div>";
-        //     echo "<p>" . $title_text . "</p>";
-        //     echo "</div>";
-        //     echo "<div class='take-text-wrapper'>";
-        //     echo "<div class='icons-title'>";
-        //     echo "<img src=" . $image_2 . ">";
-        //     echo "<h2>" . "<a>" . $title_2 . "</a>" . "</h2>";
-        //     echo "</div>";
-        //     echo "<p>" . $title_2_text . "</p>";
-        //     echo "</div>";
-        //     echo "<div class='take-text-wrapper'>";
-        //     echo "<div class='icons-title'>";
-        //     echo "<img src=" . $image_3 . ">";
-        //     echo "<h2>" . "<a>" . $title_3 . "</a>" . "</h2>";
-        //     echo "</div>";
-        //     echo "<p>" . $title_3_text . "</p>";
-        //     echo "</div>";
-        //     echo "<div class='take-text-wrapper'>";
-        //     echo "<div class='icons-title'>";
-        //     echo "<img src=" . $image_4 . ">";
-        //     echo "<h2>" . "<a>" . $title_4 . "</a>" . "</h2>";
-        //     echo "</div>";
-        //     echo "<p>" . $title_4_text . "</p>";
-        //     echo "</div>";
-        //     echo "<div class='take-text-wrapper'>";
-        //     echo "<div class='icons-title'>";
-        //     echo "<img src=" . $image_5 . ">";
-        //     echo "<h2>" . "<a>" . $title_5 . "</a>" . "</h2>";
-        //     echo "</div>";
-        //     echo "<p>" . $title_5_text . "</p>";
-        //     echo "</div>";
+            $image = CFS()->get('image');
+            $title = CFS()->get('title');
+            $title_text = CFS()->get('text');
+            $image_2 = CFS()->get('image_2');
+            $title_2 = CFS()->get('title_2');
+            $title_2_text = CFS()->get('text_2');
+            $image_3 = CFS()->get('image_3');
+            $title_3 = CFS()->get('title_3');
+            $title_3_text = CFS()->get('text_3');
+            $image_4 = CFS()->get('image_4');
+            $title_4 = CFS()->get('title_4');
+            $title_4_text = CFS()->get('text_4');
+            $image_5 = CFS()->get('image_5');
+            $title_5 = CFS()->get('title_5');
+            $title_5_text = CFS()->get('text_5');
+            echo "<div class='take-text-wrapper'>";
+            echo "<div class='icons-title'>";
+            echo "<img src=" . $image . ">";
+            echo "<h2>" . "<a>" . $title . "</a>" . "</h2>";
+            echo "</div>";
+            echo "<p>" . $title_text . "</p>";
+            echo "</div>";
+            echo "<div class='take-text-wrapper'>";
+            echo "<div class='icons-title'>";
+            echo "<img src=" . $image_2 . ">";
+            echo "<h2>" . "<a>" . $title_2 . "</a>" . "</h2>";
+            echo "</div>";
+            echo "<p>" . $title_2_text . "</p>";
+            echo "</div>";
+            echo "<div class='take-text-wrapper'>";
+            echo "<div class='icons-title'>";
+            echo "<img src=" . $image_3 . ">";
+            echo "<h2>" . "<a>" . $title_3 . "</a>" . "</h2>";
+            echo "</div>";
+            echo "<p>" . $title_3_text . "</p>";
+            echo "</div>";
+            echo "<div class='take-text-wrapper'>";
+            echo "<div class='icons-title'>";
+            echo "<img src=" . $image_4 . ">";
+            echo "<h2>" . "<a>" . $title_4 . "</a>" . "</h2>";
+            echo "</div>";
+            echo "<p>" . $title_4_text . "</p>";
+            echo "</div>";
+            echo "<div class='take-text-wrapper'>";
+            echo "<div class='icons-title'>";
+            echo "<img src=" . $image_5 . ">";
+            echo "<h2>" . "<a>" . $title_5 . "</a>" . "</h2>";
+            echo "</div>";
+            echo "<p>" . $title_5_text . "</p>";
+            echo "</div>";
             
-        //  ?>
+         ?>
     </div>
     <?php get_footer(); ?>
 </body>
+
+   
+   
+   <div class="entry-content">
+
+<?php	$args = array( 'numberposts' => '5', 'order' => 'ASC');
+    $take_part_posts = get_posts( $args ); ?>
+
+    <?php foreach ( $take_part_posts as $post ) : setup_postdata( $post );?>
+    <article class="icon-entry">
+    <?php the_post_thumbnail( 'medium' ); ?>
+    </article>
+    <?php endforeach; wp_reset_postdata(); ?>
+            
+<?php  
