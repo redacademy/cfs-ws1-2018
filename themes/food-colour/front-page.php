d3c3b172f4e10f249f14f949b2f625b5a4759875
Lucas <lucaspereiracunha08@gmail.com>
2018-07-04 20:55:11 -0700

    fixtures on design on first page. Link created on buttons

--- themes/food-colour/front-page.php
+++ themes/food-colour/front-page.php
@@ -28,7 +28,7 @@ get_header(); ?>
 				echo "<section class='title-1'>";
 					echo "<h1>" .  $fptitle_1 . "</h1>";
 					echo "<p>" . $fptext_1 . "</p>";
-					echo "<button>Exhibition</button>";
+					echo "<a href='" . get_site_url() . "/exhibition/all'><button>Exhibition</button></a>";
 				echo "</section>";
 				echo "<section class='title-2'>";
 					echo "<div>";
@@ -36,15 +36,15 @@ get_header(); ?>
 					echo "<h1>" . $fptitle_2 . "</h1>";
 					echo "</div>";
 					echo '<img src="'.esc_url($fpimage_2).'" rel="lightbox">'; 
-					echo "<p> . $fptext_2 . </p>";
-					echo "<button>About</button>";
+					echo "<p>" . $fptext_2 . "</p>";
+					echo "<a href='" . get_site_url() . "/about'><button>About</button></a>";
 				echo "</section>";
 				echo "<section class='title-3'>";
 					echo "<hr align='left'>";
 					echo "<h1>" . $fptitle_3 . "</h1>";
 					echo '<img src="'.esc_url($fpimage_3).'" rel="lightbox">'; 
-					echo "<p> . $fptext_3 . </p>";
-					echo "<button>Download Files</button>";
+					echo "<p>" . $fptext_3 . "</p>";
+					echo "<a href='https://drive.google.com/drive/folders/1KR6gb1ulL1tunhDn1yw26WX_2GnKU7-I?usp=sharing'><button>Download Files</button></a>";
 				echo "</section>";
 				echo "<hr class='bottom-line' align='center'>";
 				echo "<p class='hashtag'>#ColourFoodSecurity</p>";
