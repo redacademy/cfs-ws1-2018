e9d1300a16182b4c6a2374d3f57cd8ecaac3c897
Kachniss <vopalkova.katerina@gmail.com>
2018-07-05 18:16:28 -0700

    Add og & twitter metadata to header.php

--- themes/food-colour/header.php
+++ themes/food-colour/header.php
@@ -4,7 +4,6 @@
  *
  * @package RED_Starter_Theme
  */
-
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
@@ -12,8 +11,21 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
 
 	<?php wp_head(); ?>
+
+		<meta property="og:title" content="<?php echo the_title(); ?>">
+		<meta property="og:description" content="<?php echo CFS()->get('description'); ?>">
+		<meta property="og:type" content="article">
+		<meta property="og:url" content="<?php echo the_permalink(); ?>">
+		<meta property="og:site_name" content="<?php echo get_bloginfo(); ?>">
+		<meta property="og:image" content="<?php echo esc_url( CFS()->get('map') ); ?>">
+
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:title" content="<?php echo the_title(); ?>">
+		<meta name="twitter:description" content="<?php echo CFS()->get('description'); ?>">
+		<meta name="twitter:image" content="<?php echo esc_url( CFS()->get('map') ); ?>">
 	</head>
 
 	<body <?php body_class(); ?>>
@@ -22,12 +34,15 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<img src="<?php echo esc_url(get_bloginfo('template_directory')); ?>/assets/images/the_colours_of_food_security_logo.svg" alt="colours of food security logo"/>
+					</a>
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">Menu</button>
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
