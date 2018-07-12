ba52342a508ee5548f5c1e12005accb10460f2da
Lucas <lucaspereiracunha08@gmail.com>
2018-07-11 20:42:47 -0700

    pushing main.js changes

--- themes/food-colour/js/main.js
+++ themes/food-colour/js/main.js
@@ -52,7 +52,7 @@ jQuery(document).ready(function($) {
             });
             $('.menu-main-menu-container').css({
                 'position': 'absolute',
-                'top': '50px',
+                'top': '106px',
                 'left' : '0px',
             });
             $('#primary-menu').css({
@@ -63,7 +63,15 @@ jQuery(document).ready(function($) {
                 'height': '340px',
             });
             $('#primary-menu > li > a').css({
-                'border-width': 'thin',
+                'border-width': 'medium',
+            });
+            $('.site-branding').css({
+                'position': 'relative',
+                'top': '31px',
+            });
+            $('.menu-toggle').css({
+                'position': 'relative',
+                'top': '28px',
             });
         } else if ($('.site-header').height() >= 200) {
             $('.site-header').css({
@@ -74,6 +82,16 @@ jQuery(document).ready(function($) {
                 'display': 'none',
                 'grid-template-columns': 'none',
                 'height': 'auto',
+            });
+            $('#primary-menu > li > a').css({
+                'border-width': 'inherit',
+            });
+            $('.site-branding').css({
+                'top': '0px',
+            });
+            $('.menu-toggle').css({
+                'position': 'relative',
+                'top': '0',
             });
           }
     });
@@ -100,50 +118,45 @@ jQuery(document).ready(function($) {
      * Set height of exhibition nav icons
      */
 
-    setExhibitionNav();
 
-// smooth scroll from css tricks // for Host/Share links @ take part page
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-.not('[href="#"]')
-.not('[href="#0"]')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
+    // smooth scroll from css tricks // for Host/Share links @ take part page
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            }
+        });
         }
-      });
     }
-  }
-});
-
-    $(window).resize(function(){
-        setExhibitionNav();
     });
-    
+
     $("#forms").click(function(){
         $("#gform_1").toggle();
     });
