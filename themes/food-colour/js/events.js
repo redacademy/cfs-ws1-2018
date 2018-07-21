dba00658a1f61dad099913fa43baf6cb300f519f
Tony <diablotony01@hotmail.com>
2018-07-21 11:54:43 -0700

    add event schedule title and centered text in event end

--- themes/food-colour/js/events.js
+++ themes/food-colour/js/events.js
@@ -7,13 +7,14 @@
        eventsOnClick(this.getAttribute('event-title'), this.getAttribute('event-google'), this.getAttribute('event-facebook'), this.getAttribute('event-organization'), this.getAttribute('event-branch'), this.getAttribute('event-address'));
     });
 
-    function eventsOnClick(title, google, facebook, organization, branch, address) {
+    function eventsOnClick(title, google, facebook, organization, branch, address,) {
         $('.event-main-container').html(
             '<h2>' + title + '</h2>' +
             '<h3 class="branch-text">' + branch + '</h3>' +
             '<hr>' +
             '<p class="organization-text">' + organization + '</p>' +
             '<p>' + address + '</p>'
+            
         );
 
         if (google) {
